021c808e891c303017506cc558124281cba5bd52
Echoes Software Technologies <echoessoftwaretech@gmail.com>
2026-04-02 22:19:15 -0800

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -34,7 +34,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center p-4">
+    <div className="min-h-screen bg-white flex items-center justify-center p-4 [&_*]:!font-[Montserrat]" style={{ fontFamily: "'Montserrat', system-ui, sans-serif" }}>
       <div className="w-full max-w-md">
         {/* Logo */}
         <div className="text-center mb-8">
